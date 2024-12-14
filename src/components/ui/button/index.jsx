279bdbcfecfbb81f64e86dd84c8d15a3c080e7b4
akmaljon <akmal@gmail.com>
2024-12-14 23:46:20 +0500

    AccordionDrawer

--- src/components/ui/button/index.jsx
+++ src/components/ui/button/index.jsx
@@ -1,5 +1,13 @@
 function Button() {
-	return <div>Button</div>;
+	return (
+		<div>
+			<div className='flex gap-3'>
+				<button className='flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
+					Button
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Button;
