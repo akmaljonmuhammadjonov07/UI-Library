cef90178d143a112f57964e8241af55b203a6654
akmaljon <akmal@gmail.com>
2024-12-16 12:14:00 +0500

    Darkmodes

--- src/components/drawer/drawer.jsx
+++ src/components/drawer/drawer.jsx
@@ -11,7 +11,7 @@ function Drawer() {
 			<Sidebar />
 
 			{/* Main Content */}
-			<div className='flex md:ml-72 max-md:ml-12 bg-slate-100 w-full relative max-md:mr-96 p-8 pt-14 min-h-screen justify-start transition-all duration-300'>
+			<div className='flex md:ml-72 max-md:ml-12 dark:bg-gray-800 bg-slate-100 text-gray-800 dark:text-slate-100 w-full relative max-md:mr-96 p-8 pt-14 min-h-screen justify-start transition-all duration-300'>
 				<div className='w-full flex justify-end'>
 					<Routes>
 						<Route path='/' element={<Home />} />
